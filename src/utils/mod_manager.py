e4c8410f21c55444eae47f7a8de4b24ca6d17475
davididwhat <davidpavlic045@gmail.com>
2025-01-09 11:22:57 +0100

    Updated mod_manager.py

--- src/utils/mod_manager.py
+++ src/utils/mod_manager.py
@@ -4,14 +4,24 @@ import sys
 
 def install_modules(modules: list | str) -> None:
     if isinstance(modules, str):
+        modules = [modules]
+    elif not isinstance(modules, list):
+        raise TypeError("Modules should be a list or a string")
+    
+    if not modules or (isinstance(modules, list) and len(modules) == 0):
+        raise ValueError("No modules provided for installation")
+    
+    for module in modules:
         try:
             __import__(module)
         except ImportError:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", modules])
-    else:
-        for module in modules:
             try:
-                __import__(module)
-            except ImportError:
                 subprocess.check_call([sys.executable, "-m", "pip", "install", module])
-
+            except subprocess.CalledProcessError as e:
+                print(f"Failed to install {module}. Error: {e}")
+            except OSError as e:
+                print(f"OS error occurred while installing {module}. Error: {e}")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred while importing {module}: {e}")
