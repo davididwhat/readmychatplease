633b33f16be79f5aa5df72c5b3c2a5cc7fbe557f
davididwhat <davidpavlic045@gmail.com>
2025-01-12 17:05:25 +0100

    Moved fetch to utils...

--- src/twitch/__init__.py
+++ src/twitch/__init__.py
@@ -2,4 +2,3 @@ from .cogs.read_cog import ReaderCog
 from .twitch_bot import TwitchBot
 from .twitch_cog import TwitchCog
 from .twitch_load_cogs import load_cogs
-from .twitch_utils.get_auths import AuthFetch
