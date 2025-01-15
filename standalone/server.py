8da1be1c06b0320429eb290f0311b78b645a15b9
davididwhat <davidpavlic045@gmail.com>
2025-01-15 21:55:22 +0100

    Updated socket server to auto-assign IP address

--- standalone/server.py
+++ standalone/server.py
@@ -20,17 +20,23 @@ class Server:
                 if not data:
                     break
                 print(f"{data.decode()}")
+            
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('10.254.254.254', 1)) 
+        local_ip = s.getsockname()[0]
+    except Exception:
+        local_ip = '127.0.0.1'  
+    finally:
+        s.close()
+    return local_ip
+
+HOST = get_local_ip() 
 
 if __name__ == "__main__":
-    '''
-    Change these to what you please
-
-    If you don't know what to do, you can do this:
-    
-    HOST = "0.0.0.0"
-    PORT = 5000
-    '''
-    HOST: str       = "0.0.0.0"
+    HOST = get_local_ip() 
     PORT: int | str = 5000
 
     os.system('cls' if os.name == 'nt' else 'clear')
